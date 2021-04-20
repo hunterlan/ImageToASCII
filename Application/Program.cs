3302b341050b2a1fd7c675965cd5f280d828d351
hunterlan <kotyshe@gmail.com>
2021-04-20 20:02:38 +0300

    ImageMagick has started using

---
+++ Application/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using ImageMagick;
+
+namespace Application
+{
+    class Program
+    {
+        private readonly char[] asciiTable = {'.', ',', ':', '+', '*', '?', '%', 'S', '#', '@'};
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{args.Length} and {args}");
+
+            var pathToImage = "";
+            using (var image = new MagickImage(pathToImage))
+            {
+                image.Alpha(AlphaOption.Transparent);
+                //Separate the alpha layer from the image
+                var result = image.Separate(Channels.Alpha).First();
+
+                //Set the clip path
+                result.Clip();
+
+                //Inverse all colors inside the path
+                result.Negate();
+
+                //Copy the resulting mask back into the image as new alpha layer
+                image.Composite(result, CompositeOperator.CopyAlpha);
+
+                var acs = new char[image.Height][];
+
+                for (int i = 0; i < image.Height; i++)
+                {
+                    acs[i] = new char[image.Width];
+                    for (int j = 0; j < image.Width; j++)
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            
+        }
+    }
+}
