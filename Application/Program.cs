04d5bd6c1c9437fc96824c0622405fea5afb4d92
hunterlan <kotyshe@gmail.com>
2021-04-20 21:33:48 +0300

    it's work

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,54 +1,36 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using Core;
 using ImageMagick;
 
 namespace Application
 {
     class Program
     {
-        private static readonly char[] asciiTable = {'.', ',', ':', '+', '*', '?', '%', 'S', '#', '@'};
-        
-        static void Main(string[] args)
+        private static readonly ImageToAscii ImageToAscii = new ImageToAscii();
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine($"{args.Length} and {args}");
-
-            var pathToImage = @"C:\Users\Konstantin Sharykin\Pictures\me.jpg";
-            using (var image = new MagickImage(pathToImage))
+            if (args.Length == 2)
             {
-                image.Alpha(AlphaOption.Transparent);
-                //Separate the alpha layer from the image
-                var result = image.Separate(Channels.Alpha).First();
-
-                //Set the clip path
-                result.Clip();
-
-                //Inverse all colors inside the path
-                result.Negate();
-
-                //Copy the resulting mask back into the image as new alpha layer
-                image.Composite(result, CompositeOperator.CopyAlpha);
-
-                var pixels = image.GetPixels();
-
-                var acs = new char[image.Height][];
-
-                for (int i = 0; i < image.Height; i++)
-                {
-                    acs[i] = new char[image.Width];
-                    for (int j = 0; j < image.Width; j++)
-                    {
-                        var mapIndex = (int)Map(pixels[i, j].ToColor().R, 0, 255, 0, asciiTable.Length - 1);
-                        acs[i][j] = asciiTable[mapIndex];
-                        Console.Write(asciiTable[mapIndex]);
-                    }
-                    Console.Write('\n');
-                }
+                AutomaticMode(args);
+            }
+            else
+            {
+                Help();
             }
         }
 
-        private static float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        private static void AutomaticMode(string[] args)
         {
-            return (valueToMap - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+            var table = ImageToAscii.ConvertImageToASCII(args[0]);
+            ImageToAscii.WriteToFile(args[1], table, table.Length, table[0].Length);
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("Example of using: <script> ./test/image.img ./test/result.txt");
         }
     }
 }
---
+++ Core/ImageToASCII.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ImageMagick;
+
+namespace Core
+{
+    public class ImageToAscii
+    {
+        private readonly char[] asciiTable = {'.', ',', ':', '+', '*', '?', '%', 'S', '#', '@'};
+        
+        public char[][] ConvertImageToASCII(string path)
+        {
+            var pathToImage = path;
+            using var image = new MagickImage(pathToImage);
+            image.Alpha(AlphaOption.Transparent);
+            //Separate the alpha layer from the image
+            var result = image.Separate(Channels.Alpha).First();
+
+            //Inverse all colors inside the path
+            result.Negate();
+
+            //Copy the resulting mask back into the image as new alpha layer
+            image.Composite(result, CompositeOperator.CopyAlpha);
+
+            var pixels = image.GetPixels();
+
+            var acs = new char[image.Height][];
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                acs[i] = new char[image.Width];
+                for (int j = 0; j < image.Width; j++)
+                {
+                    var mapIndex = (int) Map(pixels[j, i].ToColor().R, 0, 255, 0, asciiTable.Length - 1);
+                    acs[i][j] = asciiTable[mapIndex];
+                }
+            }
+
+            return acs;
+        }
+        
+        private static float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return (valueToMap - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+        }
+        
+        public void WriteToFile(string path, char[][] data, int height, int weight)
+        {
+            using StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < weight; j++)
+                {
+                    sw.Write(data[i][j]);
+                }
+                sw.Write('\n');
+            }
+        }
+    }
+}
